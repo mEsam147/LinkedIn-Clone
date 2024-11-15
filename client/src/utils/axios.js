35a82a77ff675a75bf53231a11f07a45e13fa031
mEsam147 <moesam1456@gmail.com>
2024-11-15 14:07:41 +0200

    LinkedIn Project

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
-    import.meta.mode === "development" ? "http://localhost:8000/api" : "",
-    // "http://localhost:8000/api",
+    "http://localhost:8000/api",
   withCredentials: true,
 });
 
